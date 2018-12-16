8e5a2b4dad329cc84a1d6f3c8ca457f27cf7fbc7
李宇 <623763070@qq.com>
2018-12-16 20:23:12 +0800

    增加显示通知信息

--- src/main/java/com/taotieshop/demo/wechat/service/impl/PrizeServiceImpl.java
+++ src/main/java/com/taotieshop/demo/wechat/service/impl/PrizeServiceImpl.java
@@ -1,13 +1,16 @@
 package com.taotieshop.demo.wechat.service.impl;
 
 import com.taotieshop.demo.dao.PrizeMapper;
+import com.taotieshop.demo.dao.SysParameterMapper;
 import com.taotieshop.demo.entity.*;
 import com.taotieshop.demo.utils.ResultUtils;
 import com.taotieshop.demo.wechat.service.PrizeService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 任务：
@@ -21,6 +24,9 @@ public class PrizeServiceImpl implements PrizeService {
 
     @Resource
     private PrizeMapper prizeMapper;
+
+    @Resource
+    private SysParameterMapper sysParameterMapper;
     /**
      * 任务：
      * 描述：获取所有奖品信息
@@ -29,10 +35,21 @@ public class PrizeServiceImpl implements PrizeService {
     */
     @Override
     public Result getPrizeInfo() {
+        //查询奖品
         PrizeExample prizeExample = new PrizeExample();
         prizeExample.setOrderByClause("goods_order");
         List<Prize> activityList = prizeMapper.selectByExample(prizeExample);
-        return ResultUtils.success(activityList);
+
+        //查询公告
+        SysParameterExample sysParameterExample = new SysParameterExample();
+        SysParameterExample.Criteria criteria = sysParameterExample.createCriteria();
+        criteria.andPara_codeEqualTo("NOTICE_CONTENT");
+        SysParameter sysParameter =sysParameterMapper.selectByExample(sysParameterExample).get(0);
+
+        Map<String,Object> resultMap = new HashMap<String,Object>();
+        resultMap.put("prize",activityList);
+        resultMap.put("notice",sysParameter);
+        return ResultUtils.success(resultMap);
     }
 
     /**
